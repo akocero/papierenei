0e572773b979b58d467301f0e7c3d09bb7d2065e
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-08-06 19:52:26 +0800

    Implement dynamic token

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-// import store from '@/store';
+import Cookies from 'js-cookie';
+
+const token = Cookies.get('token') || null;
 
 const axiosConfig = axios.create({
 	baseURL: 'http://localhost:4000/api/v1/',
 	headers: {
-		Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzkxYzVjZDJlY2E5OTBmMzRiZmNiYSIsImlhdCI6MTY1OTYxMzE1OCwiZXhwIjoxNjU5Njk5NTU4fQ.a76Cvp1AqI0MqKdotdH4kQuXYKQiHp_vLw4m0K2u8qE`,
+		Authorization: `Bearer ${token}`,
 	},
 });
 
