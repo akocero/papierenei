fb91a5d2adca56135a329b8a5fe2b808a376b02f
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-08-06 19:51:56 +0800

    Added auth layout for authentication

---
+++ src/components/layouts/Auth.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<AlertList :alerts="alerts" />
+		<RouterView />
+	</div>
+</template>
+
+<script setup>
+import AlertList from '../AlertList.vue';
+import useAlert from '@/composables/useAlert.js';
+import { ref } from 'vue';
+
+const { alerts } = useAlert();
+</script>
