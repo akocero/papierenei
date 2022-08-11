68834889b99b8f391adcbddbc9474cfb5130cca1
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-08-11 19:57:48 +0800

    feat: added rewrites

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,12 +1,14 @@
 <template>
 	<span
-		class="rounded-full px-2 text-white"
+		class="rounded-full border px-2 font-semibold text-white"
 		:class="[
 			{
-				'bg-yellow-500': color === 'warning',
+				'border-yellow-500 bg-yellow-300/40 text-yellow-700':
+					color === 'warning',
 				'bg-red-500': color === 'danger',
 				'bg-blue-500': color === 'info',
-				'bg-green-500': color === 'success',
+				'border-green-500 bg-green-300/70 text-green-700':
+					color === 'success',
 				'bg-gray-500': color === '',
 			},
 		]"
