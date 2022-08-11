707951e256dc6f58fde57734cf598a48abddc082
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-08-11 23:49:40 +0800

    feat: fix axios config

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 const token = Cookies.get('token') || null;
 
 const axiosConfig = axios.create({
-	baseURL: 'http://localhost:4000/api/v1/',
+	baseURL: 'https://papierenei.herokuapp.com/api/v1/',
 	headers: {
 		Authorization: `Bearer ${token}`,
 	},
