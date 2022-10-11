ebfec04d55da1ca72bafadbfcfc4ea04ff3ead38
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-10-11 20:50:26 +0800

    Create ecomm_setting.js

---
+++ src/stores/ecomm_setting.js
@@ -0,0 +1,45 @@
+import { defineStore } from 'pinia';
+import storeHelpers from '../helpers/storeHelpers';
+
+export const useEcommSettingStore = defineStore({
+	id: 'ecommSetting',
+	state: () => ({
+		list: [],
+		isLoading: false,
+		item: null,
+		page: 1,
+		url: `ecomm_settings`,
+		response: null,
+		error: null,
+	}),
+	getters: {
+		doubleCount: (state) => state.counter * 2,
+	},
+	actions: {
+		async init() {
+			this.create(this, {
+				banners: [
+					{
+						text: '30 per',
+						name: '30',
+					},
+				],
+			});
+		},
+		async create(payload) {
+			await storeHelpers.create(this, payload);
+		},
+		async update(payload) {
+			return await storeHelpers.update(this, payload);
+		},
+		async fetch(query) {
+			await storeHelpers.fetch(this, query);
+		},
+		async find(id) {
+			await storeHelpers.find(this, id);
+		},
+		async deleteImage(payload) {
+			await storeHelpers.deleteImage(this, payload);
+		},
+	},
+});
