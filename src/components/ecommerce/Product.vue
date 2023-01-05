e42d27dc7a3b1846fc65fe6535033c0514fdfd60
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2023-01-06 00:55:59 +0800

    Update Product UI

--- src/components/ecommerce/Product.vue
+++ src/components/ecommerce/Product.vue
@@ -1,19 +1,19 @@
 <template>
-	<div class="relative rounded-lg border p-5 text-left shadow" v-if="product">
+	<div class="relative rounded-lg p-5 text-left text-gray-800" v-if="product">
 		<span
-			class="absolute left-3 top-3 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-red-400 px-2 py-1 text-sm font-bold text-white"
+			class="absolute left-0 top-0 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-red-400 px-2 py-1 text-sm font-bold text-white"
 			v-if="product.salePrice"
 			>SALE!</span
 		>
 
 		<span
-			class="absolute right-3 top-3 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-darkYellow px-2 py-1 text-sm font-bold text-white"
+			class="absolute right-0 top-0 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-darkYellow px-2 py-1 text-sm font-bold text-white"
 			>NEW!</span
 		>
 		<div class="group relative">
 			<div
 				v-if="product.images.length || product.coverPhoto"
-				class="relative h-52 w-full"
+				class="relative h-64 w-full"
 			>
 				<div
 					v-if="!product.quantity"
@@ -35,7 +35,7 @@
 				/>
 			</div>
 
-			<div v-else class="h-52 w-full">
+			<div v-else class="h-64 w-full">
 				<div
 					v-if="!product.quantity"
 					class="absolute left-1/2 top-1/2 flex h-20 w-20 -translate-x-2/4 -translate-y-2/4 transform items-center justify-center rounded-full bg-gray-900/50 text-sm font-bold text-white"
@@ -57,10 +57,10 @@
 			</button>
 		</div>
 
-		<div class="mt-2">
-			<h4 class="font-bold">{{ product.name }}</h4>
+		<div class="mt-2 text-center text-xl font-bold">
+			<h4 class="capitalize">{{ product.name }}</h4>
 
-			<h5 class="font-mono text-2xl" v-if="!product.salePrice">
+			<h5 class="font-mono" v-if="!product.salePrice">
 				₱{{ numberFormat(product.unitCost) }}
 			</h5>
 
@@ -68,7 +68,9 @@
 				<span class="text-lg text-gray-400 line-through"
 					>₱{{ numberFormat(product.unitCost) }}</span
 				>
-				₱{{ numberFormat(product.salePrice) }}
+				<span class="text-red-400">
+					₱{{ numberFormat(product.salePrice) }}
+				</span>
 			</h5>
 
 			<p class="my-2 text-sm font-bold text-green-600">
@@ -78,7 +80,7 @@
 			</p>
 
 			<button
-				class="w-full rounded-lg bg-darkBlue px-6 py-2 font-bold text-white"
+				class="w-full bg-darkBlue px-6 py-2 text-sm font-bold text-white"
 				@click="$emit('addToCart', product)"
 				v-if="product.quantity"
 			>
