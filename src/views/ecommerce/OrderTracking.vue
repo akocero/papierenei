e776f8bbbf7f5f5303d48f5c9cccfa196e73144e
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2023-03-12 18:08:37 +0800

    terms

--- src/views/ecommerce/OrderTracking.vue
+++ src/views/ecommerce/OrderTracking.vue
@@ -3,7 +3,7 @@
 		<h1 class="my-5 text-5xl font-bold">Order Tracking</h1>
 		<ul>
 			<li>
-				<h3>
+				<h3 class="mt-6 mb-2 font-bold">
 					After the order payment is completed, how can I determine
 					whether my payment is successful?
 				</h3>
@@ -13,11 +13,13 @@
 				</p>
 			</li>
 			<li>
-				<h3>When can my order be sent out?</h3>
+				<h3 class="mt-6 mb-2 font-bold">
+					When can my order be sent out?
+				</h3>
 				<p>Each package will be sent out within 1-3 days.</p>
 			</li>
 			<li>
-				<h3>How do I track my package?</h3>
+				<h3 class="mt-6 mb-2 font-bold">How do I track my package?</h3>
 				<p>
 					We will send out a tracking number along with your invoice
 					to your email.
